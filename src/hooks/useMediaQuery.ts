785ddd0fd0c0c78b0b0bef22de11d5a2452d55f8
Irfan Dayan <engr.irfandayan@gmail.com>
2022-12-22 20:05:25 +0500

    Add media Query API

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -9,6 +9,7 @@ const useMediaQuery = (query: string) => {
       setMatches(media.matches);
     }
     const listener = () => setMatches(media.matches);
+
     window.addEventListener("resize", listener);
     return () => window.removeEventListener("resize", listener);
   }, [matches, query]);
